23e5e732fb6ea35627e4684f2d8e4b91a48d8c15
Brian Parker <brian.parker@hotmail.com.au>
2020-09-08 23:09:35 +1000

    New default base class for all components.

---
+++ BlazorLocalisation/Models/ApplicationComponentBase.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorLocalisation.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.Extensions.Localization;
+
+    public class ApplicationComponentBase : ComponentBase
+    {
+        [Inject]
+        public IStringLocalizer<App> L { get; set; }
+    }
+}
